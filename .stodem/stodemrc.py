a07c3df9b68f8c35796ca8af736a26a7ea687450
Paul Rulis <rulisp@umkc.edu>
2024-12-29 09:27:08 -0600

    First commit

---
+++ .stodem/stodemrc.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+import os
+
+
+def parameters_and_defaults():
+    param_dict = {
+            "infile" : "stodem.in.xml" # String
+            }
+    return param_dict
+
+
+if __name__ == '__main__':
+    print(parameters_and_defaults())
